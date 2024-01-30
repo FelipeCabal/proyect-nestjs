0c159d97d8885d5144a8b987dfca87bd8d12a918
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-01-30 12:29:18 -0500

    Terminamos la validacion

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,30 @@
 import {TaskStatus} from '../tasks.entity'
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator'
 
 export class CreateTaskDto{
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string;
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
 }
 
 export class UpdateTaskDto{
-    title?: string
-    description?: string
-    status?: TaskStatus
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    title?: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    description?: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    status?: TaskStatus;
 }
